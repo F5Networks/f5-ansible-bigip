49670ddec812165ad6a13b9eb05bd4c50a2243c9
wypior <w.wypior@f5.com>
2022-06-06 01:00:48 +0000

    Merging devel changes - 2022-06-06T01:00:33Z

--- ansible_collections/f5networks/f5_bigip/plugins/modules/bigip_sslo_service_tap.py
+++ ansible_collections/f5networks/f5_bigip/plugins/modules/bigip_sslo_service_tap.py
@@ -168,14 +168,14 @@ from ..module_utils.client import (
     F5Client, sslo_version
 )
 from ..module_utils.common import (
-    F5ModuleError, AnsibleF5Parameters, process_json, flatten_boolean
+    F5ModuleError, AnsibleF5Parameters, process_json
 )
 
 from ..module_utils.constants import (
-    min_sslo_version, max_sslo_version, json_enable_tls13
+    min_sslo_version, max_sslo_version
 )
 
-from ..module_utils.compare import compare_complex_list, compare_dictionary
+from ..module_utils.compare import compare_dictionary
 from ..module_utils.sslo_templates.sslo_service_tap import (
     create_modify, delete
 )
