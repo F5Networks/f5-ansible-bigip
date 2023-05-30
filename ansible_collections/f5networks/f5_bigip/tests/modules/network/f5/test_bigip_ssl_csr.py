34746112622279c20618ef80a1abf4bfc8fd0e3b
wypior <w.wypior@f5.com>
2023-05-30 01:00:57 +0000

    Merging devel changes - 2023-05-30T01:00:45Z

--- ansible_collections/f5networks/f5_bigip/tests/modules/network/f5/test_bigip_ssl_csr.py
+++ ansible_collections/f5networks/f5_bigip/tests/modules/network/f5/test_bigip_ssl_csr.py
@@ -11,12 +11,16 @@ import os
 
 from ansible.module_utils.basic import AnsibleModule
 
+from ansible_collections.f5networks.f5_bigip.plugins.modules import bigip_ssl_csr
 from ansible_collections.f5networks.f5_bigip.plugins.modules.bigip_ssl_csr import (
     ModuleParameters, ApiParameters, ArgumentSpec, ModuleManager
 )
+from ansible_collections.f5networks.f5_bigip.plugins.module_utils.common import F5ModuleError
 from ansible_collections.f5networks.f5_bigip.tests.compat import unittest
 from ansible_collections.f5networks.f5_bigip.tests.compat.mock import Mock, patch
-from ansible_collections.f5networks.f5_bigip.tests.modules.utils import set_module_args
+from ansible_collections.f5networks.f5_bigip.tests.modules.utils import (
+    set_module_args, AnsibleExitJson, AnsibleFailJson, exit_json, fail_json
+)
 
 
 fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
@@ -63,23 +67,39 @@ class TestParameters(unittest.TestCase):
         assert p.common_name == 'ssl_csr_1'
 
 
-class TestManager(unittest.TestCase):
+class TestModuleManager(unittest.TestCase):
     def setUp(self):
         self.spec = ArgumentSpec()
         self.p1 = patch('ansible_collections.f5networks.f5_bigip.plugins.modules.bigip_ssl_csr.send_teem')
+        self.p2 = patch('ansible_collections.f5networks.f5_bigip.plugins.modules.bigip_ssl_csr.tmos_version')
+        self.p3 = patch('ansible_collections.f5networks.f5_bigip.plugins.modules.bigip_ssl_csr.F5Client')
+        self.p4 = patch('os.path.exists')
+        self.m4 = self.p4.start()
+        self.m4.return_value = True
         self.m1 = self.p1.start()
         self.m1.return_value = True
+        self.m2 = self.p2.start()
+        self.m2.return_value = '14.1.0'
+        self.m3 = self.p3.start()
+        self.m3.return_value = Mock()
+        self.mock_module_helper = patch.multiple(AnsibleModule,
+                                                 exit_json=exit_json,
+                                                 fail_json=fail_json)
+        self.mock_module_helper.start()
 
     def tearDown(self):
         self.p1.stop()
+        self.p2.stop()
+        self.p3.stop()
+        self.mock_module_helper.stop()
 
-    def test_create(self, *args):
+    def test_create_csr_success(self, *args):
         set_module_args(dict(
             name='ssl_csr_1',
-            common_name='ssl_csr_1',
-            key_name='ssl_key_1',
-            dest='/tmp/ssl_csr_1',
-            force=True,
+            common_name='b4',
+            key_name='default.key',
+            dest='/foo/ssl_csr_1',
+            province='foobar',
             state='present'
         ))
 
@@ -90,17 +110,270 @@ class TestManager(unittest.TestCase):
 
         # Override methods in the specific type of manager
         mm = ModuleManager(module=module)
-        mm.version_is_less_than_14 = Mock(return_value=False)
         mm.exists = Mock(return_value=False)
-        mm.file_exists = Mock(return_value=False)
-        mm.create_on_device = Mock(return_value=True)
-        mm._move_csr_to_download = Mock(return_value=True)
-        mm._delete_csr = Mock(return_value=True)
-        mm._download_file = Mock(return_value=True)
-        mm.remove_from_device = Mock(return_value=True)
+        mm.client.plugin.download_file = Mock()
+        mm.client.post.side_effect = [
+            {'code': 200, 'contents': {}},
+            {'code': 200, 'contents': {}},
+            {'code': 200, 'contents': {}},
+            {'code': 200, 'contents': load_fixture('csr_file_does_not_exist.json')},
+        ]
 
-        with patch('os.path.exists') as mo:
-            mo.return_value = True
-            results = mm.exec_module()
+        results = mm.exec_module()
 
-        assert results['changed'] is True
+        self.assertTrue(results['changed'])
+        self.assertTrue(results['common_name'], 'b4')
+
+    def test_remove_csr_success(self, *args):
+        set_module_args(dict(
+            name='ssl_csr_1',
+            dest='/tmp/ssl_csr_1',
+            state='absent'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+
+        # Override methods in the specific type of manager
+        mm = ModuleManager(module=module)
+        mm.exists = Mock(side_effect=[True, False])
+        mm.client.delete.return_value = dict(code=200, contents={})
+
+        results = mm.exec_module()
+
+        self.assertTrue(results['changed'])
+
+    @patch.object(bigip_ssl_csr, 'Connection')
+    @patch.object(bigip_ssl_csr.ModuleManager, 'exec_module',
+                  Mock(return_value={'changed': False})
+                  )
+    def test_main_function_success(self, *args):
+        set_module_args(dict(
+            name='ssl_csr_1',
+            common_name='ssl_csr_1',
+            key_name='ssl_key_1',
+            dest='/tmp/ssl_csr_1',
+        ))
+
+        with self.assertRaises(AnsibleExitJson) as result:
+            bigip_ssl_csr.main()
+
+        self.assertFalse(result.exception.args[0]['changed'])
+
+    @patch.object(bigip_ssl_csr, 'Connection')
+    @patch.object(bigip_ssl_csr.ModuleManager, 'exec_module',
+                  Mock(side_effect=F5ModuleError('This module has failed.'))
+                  )
+    def test_main_function_failed(self, *args):
+        set_module_args(dict(
+            name='ssl_csr_1',
+            common_name='ssl_csr_1',
+            key_name='ssl_key_1',
+            dest='/tmp/ssl_csr_1',
+        ))
+
+        with self.assertRaises(AnsibleFailJson) as result:
+            bigip_ssl_csr.main()
+
+        self.assertTrue(result.exception.args[0]['failed'])
+        self.assertIn('This module has failed', result.exception.args[0]['msg'])
+
+    @patch.object(bigip_ssl_csr, 'HAS_PACKAGING', False)
+    @patch.object(bigip_ssl_csr, 'Connection')
+    @patch.object(bigip_ssl_csr.ModuleManager, 'exec_module',
+                  Mock(side_effect=F5ModuleError('This module has failed.'))
+                  )
+    def test_main_function_import_error(self, *args):
+        set_module_args(dict(
+            name='ssl_csr_1',
+            common_name='ssl_csr_1',
+            key_name='ssl_key_1',
+            dest='/tmp/ssl_csr_1',
+        ))
+
+        with self.assertRaises(AnsibleFailJson) as result:
+            bigip_ssl_csr.PACKAGING_IMPORT_ERROR = "failed to import the 'packaging' package"
+            bigip_ssl_csr.main()
+
+        self.assertTrue(result.exception.args[0]['failed'])
+        self.assertIn(
+            'Failed to import the required Python library (packaging)',
+            result.exception.args[0]['msg']
+        )
+
+    def test_execute_errors(self):
+        set_module_args(dict(
+            name='ssl_csr_1',
+            common_name='b4',
+            key_name='default.key',
+            dest='/tmp/ssl_csr_1',
+            state='present'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+
+        # Override methods in the specific type of manager
+        mm = ModuleManager(module=module)
+        mm.want.force = Mock(return_value=False)
+
+        with self.assertRaises(F5ModuleError) as err1:
+            mm.create = Mock(return_value=False)
+            mm.execute()
+
+        self.assertIn('Failed to create csr on device', err1.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err2:
+            mm.create = Mock(return_value=True)
+            mm._move_csr_to_download = Mock(return_value=False)
+            mm.execute()
+
+        self.assertIn('Failed to move the csr file to a downloadable location', err2.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err3:
+            mm.create = Mock(return_value=True)
+            mm._move_csr_to_download = Mock(return_value=True)
+            mm._download_file = Mock(return_value=False)
+            mm.execute()
+
+        self.assertIn('Failed to save the csr file to local disk', err3.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err4:
+            mm.create = Mock(return_value=True)
+            mm._move_csr_to_download = Mock(return_value=True)
+            mm._download_file = Mock(return_value=True)
+            mm._delete_csr = Mock(return_value=True)
+            mm.file_exists = Mock(return_value=True)
+            mm.execute()
+
+        self.assertIn('Failed to remove the remote csr file', err4.exception.args[0])
+
+    def test_on_device_methods(self):
+        set_module_args(dict(
+            name='ssl_csr_1',
+            common_name='b4',
+            key_name='default.key',
+            dest='/tmp/ssl_csr_1',
+            state='present'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+
+        # Override methods in the specific type of manager
+        mm = ModuleManager(module=module)
+
+        mm.client.get.side_effect = [
+            dict(code=200, contents={}),
+            dict(code=404, contents={}),
+            dict(code=401, contents={'unauthorized'})
+        ]
+
+        self.assertTrue(mm.exists())
+        self.assertFalse(mm.exists())
+
+        with self.assertRaises(F5ModuleError) as err1:
+            mm.exists()
+
+        self.assertIn("unauthorized", err1.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err2:
+            mm.client.post.return_value = dict(code=401, contents={'unauthorized'})
+            mm.create_on_device()
+
+        self.assertIn("unauthorized", err2.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err3:
+            mm.client.delete.return_value = dict(code=401, contents={'unauthorized'})
+            mm.remove_from_device()
+
+        self.assertIn("unauthorized", err3.exception.args[0])
+
+        mm.client.post.side_effect = [
+            dict(code=404, contents={}),
+            dict(code=401, contents='unauthorized'),
+            dict(code=200, contents=load_fixture('csr_file_exists.json')),
+            dict(code=200, contents={}),
+        ]
+
+        self.assertFalse(mm.file_exists())
+
+        with self.assertRaises(F5ModuleError) as err4:
+            mm.file_exists()
+
+        self.assertIn("unauthorized", err4.exception.args[0])
+
+        self.assertTrue(mm.file_exists())
+        self.assertFalse(mm.file_exists())
+
+        mm.client.plugin.download_file = Mock()
+
+        self.m4.return_value = False
+        self.assertFalse(mm._download_file())
+
+        mm.client.post.side_effect = [
+            dict(code=404, contents={}),
+            dict(code=401, contents='unauthorized'),
+            dict(code=403, contents='forbidden'),
+        ]
+
+        self.assertFalse(mm._delete_csr())
+
+        with self.assertRaises(F5ModuleError) as err5:
+            mm._delete_csr()
+
+        self.assertIn("unauthorized", err5.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err6:
+            mm._move_csr_to_download()
+
+        self.assertIn("forbidden", err6.exception.args[0])
+
+    def test_remaining_methods(self):
+        set_module_args(dict(
+            name='ssl_csr_1',
+            common_name='b4',
+            key_name='default.key',
+            dest='/tmp/ssl_csr_1',
+            state='present'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+
+        # Override methods in the specific type of manager
+        mm = ModuleManager(module=module)
+        self.m4 = self.p4.start()
+
+        with self.assertRaises(F5ModuleError) as err1:
+            self.m4.return_value = False
+            mm.present()
+        self.assertIn("The directory of your 'dest' file does not exist", err1.exception.args[0])
+
+        self.m4.return_value = True
+        mm.exists = Mock(return_value=True)
+        self.assertFalse(mm.present())
+
+        with self.assertRaises(F5ModuleError) as err2:
+            self.m2.return_value = '13.1.0'
+            mm.exec_module()
+
+        self.assertIn('This module requires TMOS version 14.x and above', err2.exception.args[0])
+
+        mm.exists = Mock(return_value=False)
+        self.assertFalse(mm.absent())
+
+        with self.assertRaises(F5ModuleError) as err2:
+            mm.remove_from_device = Mock(return_value=True)
+            mm.exists = Mock(return_value=True)
+            mm.remove()
+
+        self.assertIn('Failed to delete the resourc', err2.exception.args[0])
--- ansible_collections/f5networks/f5_bigip/tests/modules/network/f5/test_bigip_ssl_pkcs12.py
+++ ansible_collections/f5networks/f5_bigip/tests/modules/network/f5/test_bigip_ssl_pkcs12.py
@@ -11,13 +11,16 @@ import os
 
 from ansible.module_utils.basic import AnsibleModule
 
+from ansible_collections.f5networks.f5_bigip.plugins.modules import bigip_ssl_pkcs12
 from ansible_collections.f5networks.f5_bigip.plugins.modules.bigip_ssl_pkcs12 import (
     ModuleParameters, ArgumentSpec, ModuleManager
 )
+from ansible_collections.f5networks.f5_bigip.plugins.module_utils.common import F5ModuleError
 from ansible_collections.f5networks.f5_bigip.tests.compat import unittest
 from ansible_collections.f5networks.f5_bigip.tests.compat.mock import Mock, patch
-from ansible_collections.f5networks.f5_bigip.tests.modules.utils import set_module_args
-
+from ansible_collections.f5networks.f5_bigip.tests.modules.utils import (
+    set_module_args, AnsibleExitJson, AnsibleFailJson, exit_json, fail_json
+)
 
 fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 fixture_data = {}
@@ -59,11 +62,20 @@ class TestManager(unittest.TestCase):
     def setUp(self):
         self.spec = ArgumentSpec()
         self.p1 = patch('ansible_collections.f5networks.f5_bigip.plugins.modules.bigip_ssl_pkcs12.send_teem')
+        self.p2 = patch('ansible_collections.f5networks.f5_bigip.plugins.modules.bigip_ssl_pkcs12.F5Client')
         self.m1 = self.p1.start()
         self.m1.return_value = True
+        self.m2 = self.p2.start()
+        self.m2.return_value = Mock()
+        self.mock_module_helper = patch.multiple(AnsibleModule,
+                                                 exit_json=exit_json,
+                                                 fail_json=fail_json)
+        self.mock_module_helper.start()
 
     def tearDown(self):
         self.p1.stop()
+        self.p2.stop()
+        self.mock_module_helper.stop()
 
     def test_import_from_file(self, *args):
         set_module_args(dict(
@@ -80,11 +92,144 @@ class TestManager(unittest.TestCase):
         # Override methods to force specific logic in the module to happen
         mm = ModuleManager(module=module)
         mm.exists = Mock(return_value=False)
-        mm.install_on_device = Mock(return_value=True)
-        mm.remove_temp_file_from_device = Mock(return_value=True)
+        mm.client.upload_file = Mock()
+        mm.client.post.return_value = dict(code=200, contents={})
 
         results = mm.exec_module()
 
-        assert results['changed'] is True
-        assert results['name'] == 'fake_cert'
-        assert results['source'] == '/var/fake/fake.p12'
+        self.assertTrue(results['changed'])
+        self.assertEqual(results['name'], 'fake_cert')
+        self.assertTrue(results['source'], '/var/fake/fake.p12')
+        self.assertTrue(mm.client.post.call_count == 2)
+
+    def test_remove_from_device(self, *args):
+        set_module_args(dict(
+            name='fake_cert',
+            state='absent'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+
+        # Override methods to force specific logic in the module to happen
+        mm = ModuleManager(module=module)
+        mm.exists = Mock(side_effect=[True, False])
+        mm.client.delete.return_value = dict(code=200, contents={})
+
+        results = mm.exec_module()
+
+        self.assertTrue(results['changed'])
+        self.assertTrue(mm.client.delete.call_count == 2)
+
+    @patch.object(bigip_ssl_pkcs12, 'Connection')
+    @patch.object(bigip_ssl_pkcs12.ModuleManager, 'exec_module',
+                  Mock(return_value={'changed': False})
+                  )
+    def test_main_function_success(self, *args):
+        set_module_args(dict(
+            name='ssl_key_1'
+        ))
+
+        with self.assertRaises(AnsibleExitJson) as result:
+            bigip_ssl_pkcs12.main()
+
+        self.assertFalse(result.exception.args[0]['changed'])
+
+    @patch.object(bigip_ssl_pkcs12, 'Connection')
+    @patch.object(bigip_ssl_pkcs12.ModuleManager, 'exec_module',
+                  Mock(side_effect=F5ModuleError('This module has failed.'))
+                  )
+    def test_main_function_failed(self, *args):
+        set_module_args(dict(
+            name='ssl_key_1'
+        ))
+
+        with self.assertRaises(AnsibleFailJson) as result:
+            bigip_ssl_pkcs12.main()
+
+        self.assertTrue(result.exception.args[0]['failed'])
+        self.assertIn('This module has failed', result.exception.args[0]['msg'])
+
+    def test_on_device_methods(self):
+        set_module_args(dict(
+            name='fake_cert',
+            force=False,
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+
+        # Override methods to force specific logic in the module to happen
+        mm = ModuleManager(module=module)
+
+        mm.client.post.side_effect = [
+            dict(code=500, contents='server error'),
+            dict(code=401, contents='unauthorized')
+        ]
+
+        with self.assertRaises(F5ModuleError) as err1:
+            mm.remove_temp_file_from_device()
+        self.assertIn('server error', err1.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err2:
+            mm.install_on_device()
+        self.assertIn('unauthorized', err2.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err3:
+            mm.client.plugin.upload_file = Mock(side_effect=F5ModuleError('upload failed'))
+            mm.upload_file_to_device('foo', 'bar')
+
+        self.assertIn('Failed to upload the file', err3.exception.args[0])
+
+        mm.client.delete.side_effect = [
+            dict(code=401, contents='unauthorized'),
+            dict(code=200, contents={}),
+            dict(code=200, contents={}),
+            dict(code=404, contents='not found'),
+        ]
+
+        with self.assertRaises(F5ModuleError) as err4:
+            mm.remove_from_device()
+        self.assertIn('unauthorized', err4.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err5:
+            mm.remove_from_device()
+        self.assertIn('not found', err5.exception.args[0])
+
+        mm.client.get.side_effect = [
+            dict(code=404, contents='not found'),
+            dict(code=200, contents={}),
+            dict(code=401, contents='unauthorized'),
+            dict(code=200, contents={}),
+            dict(code=200, contents={}),
+            dict(code=401, contents='unauthorized'),
+            dict(code=200, contents={}),
+            dict(code=200, contents={}),
+        ]
+
+        self.assertFalse(mm.exists())
+
+        with self.assertRaises(F5ModuleError) as err5:
+            mm.exists()
+        self.assertIn('unauthorized', err5.exception.args[0])
+
+        with self.assertRaises(F5ModuleError) as err6:
+            mm.exists()
+        self.assertIn('unauthorized', err6.exception.args[0])
+
+        self.assertTrue(mm.exists())
+
+        mm.exists = Mock(side_effect=[True, False, True])
+        mm.remove_from_device = Mock(return_value=True)
+        with self.assertRaises(F5ModuleError) as err7:
+            mm.remove()
+
+        self.assertIn('Failed to delete the resource', err7.exception.args[0])
+
+        self.assertFalse(mm.absent())
+
+        self.assertFalse(mm.present())
